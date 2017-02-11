975a370db1072118c4810366796822f8bb48dc2c
Austin Heiman <atheimanksu@gmail.com>
2017-02-10 21:34:42 -0600

    linting is dumb

--- .travis.sh
+++ .travis.sh
@@ -4,10 +4,6 @@ case $SUITE in
 chefspec)
   rspec
   ;;
-lint)
-  foodcritic --context --progress .
-  rubocop --lint --display-style-guide --extra-details --display-cop-names
-  ;;
 *)
   KITCHEN_LOCAL_YAML='.kitchen.docker.yml' kitchen test "$SUITE" --concurrency=2 --log-level=debug
   ;;
